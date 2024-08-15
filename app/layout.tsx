bfa41ac3ad9573533f0ad1d0bd0b749a33160f44
x_yue <103266340+yuepem@users.noreply.github.com>
2024-08-15 16:14:33 +0200

    Style: set body to take the full height of the viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}  >
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <header>
           <NavBar />
         </header>
-        {children}
+        <main className="flex-grow">
+          {children}
+        </main>
         <footer>
           <Footer />
         </footer>
